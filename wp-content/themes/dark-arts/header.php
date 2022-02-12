23845aa5dc11d25374f0f6268ab55d4e6e53b2b7
Luna Bee <lunabbarber@gmail.com>
2022-02-11 22:39:56 -0600

    build: updates to WP 5.9

--- wp-content/themes/dark-arts/header.php
+++ wp-content/themes/dark-arts/header.php
@@ -41,6 +41,7 @@
     <div class="container">
 
       <div class="blog-header">
-        <h1 class="blog-title"><a href="<?php echo get_bloginfo ( 'wpurl' );?>"><?php echo get_bloginfo( 'name' );?></h1>
+      <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
         <p class="lead blog-description"><?php echo get_bloginfo( 'description' );?></p>
       </div>
+      
