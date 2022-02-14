fbcc0cb8ecf71f98d1e168ce3674804696536cec
Luna Bee <lunabbarber@gmail.com>
2022-02-13 18:21:21 -0600

    feat(image): Add post thumbnail to content and content single php files

--- wp-content/themes/dark-arts/content.php
+++ wp-content/themes/dark-arts/content.php
@@ -8,6 +8,17 @@
     </a>
   </p>
 
-  <?php the_excerpt(); ?>
+  <?php if ( has_post_thumbnail() ) {?>
+	<div class="row">
+		<div class="col-md-4">
+			<?php	the_post_thumbnail('thumbnail'); ?>
+		</div>
+		<div class="col-md-6">
+			<?php the_excerpt(); ?>
+		</div>
+	</div>
+	<?php } else { ?>
+	<?php the_excerpt(); ?>
+<?php } ?>
 
 </div><!-- /.blog-post -->
